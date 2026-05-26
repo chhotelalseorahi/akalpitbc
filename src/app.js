85b929bcad5e6fe22fc64a9c3f3500f01f260bb5
256bitguy <ashishrajsri2233@gmail.com>
2026-05-26 12:56:34 +0530

    base url change

--- src/app.js
+++ src/app.js
@@ -109,7 +109,7 @@ app.get("/.well-known/assetlinks.json", (req, res) => {
         package_name: "com.amiable_ventures.akalpit",
         sha256_cert_fingerprints: [
           "4D:6E:79:93:2B:78:E0:8D:B0:DF:D0:99:65:31:C6:81:6B:16:64:66:AA:0B:A5:62:B9:51:AA:CA:34:39:E0:60",
-          // "B1:E5:B4:B0:D5:DC:D7:8C:DC:0C:03:E2:B9:F1:53:95:78:74:27:DD:DC:DB:80:FF:E0:CA:C9:6E:12:B1:C1:4C",
+          "B1:E5:B4:B0:D5:DC:D7:8C:DC:0C:03:E2:B9:F1:53:95:78:74:27:DD:DC:DB:80:FF:E0:CA:C9:6E:12:B1:C1:4C",
         ],
       },
     },
@@ -122,7 +122,7 @@ const PLAY_STORE_URL =
   "https://play.google.com/store/apps/details?id=com.amiable_ventures.akalpit";
 
 const APP_STORE_URL  = "https://apps.apple.com/app/akalpit/id000000000"; // replace
-const BASE_URL       = "https://api.akalpit.in";
+const BASE_URL       = "https://link.akalpit.in";
 
 function deepLinkPage({ title, description, path, imageUrl }) {
   const appUrl    = `${BASE_URL}${path}`;
